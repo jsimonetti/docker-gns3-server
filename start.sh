d7259a0ef4377dd3ac28e0a83accbd0ab6326296
joepasss@gentoo <farwes1180@gmail.com>
2025-05-19 06:31:07 +0900

    add double quote

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 if [ "${CONFIG}x" == "x" ]; then
-	CONFIG=/data/config.ini
+  CONFIG=/data/config.ini
 fi
 
-if [ ! -e $CONFIG ]; then
-	cp /config.ini /data
+if [ ! -e "$CONFIG" ]; then
+  cp /config.ini /data
 fi
 
 brctl addbr virbr0
@@ -12,9 +12,9 @@ ip link set dev virbr0 up
 if [ "${BRIDGE_ADDRESS}x" == "x" ]; then
   BRIDGE_ADDRESS=172.21.1.1/24
 fi
-ip ad add ${BRIDGE_ADDRESS} dev virbr0
+ip ad add "${BRIDGE_ADDRESS}" dev virbr0
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 dnsmasq -i virbr0 -z -h --dhcp-range=172.21.1.10,172.21.1.250,4h
 dockerd --storage-driver=vfs --data-root=/data/docker/ &
-gns3server -A --config $CONFIG
+gns3server -A --config "$CONFIG"
